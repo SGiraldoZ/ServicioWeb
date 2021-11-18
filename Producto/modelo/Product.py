598b4abd5fdd393270c0fb8b2b59e229a6fdc403
SGiraldoZ <sebasgizu@gmail.com>
2021-11-17 19:18:23 -0500

    Product class

---
+++ Producto/modelo/Product.py
@@ -0,0 +1,40 @@
+import json
+
+class Product:
+    def __init__(self,id, name, cantidad, precio, ownerId):
+        self.name = name
+        self.cantidad = cantidad
+        self.precio = precio
+        self.ownerId = ownerId
+        
+    @staticmethod
+    def from_Json(json):
+        if json:
+            if "nombre" in json:
+                nombre = json['nombre']
+            else:
+                return None
+            if "cantidad" in json:
+                cantidad = json['cantidad']
+            else:
+                return None
+            if "precio" in json:
+                precio = json['precio']
+            else:
+                return None
+            if "ownerId" in json:
+                ownerId = json['ownerId']
+            else:
+                return None
+        return Product(nombre, cantidad, precio, ownerId)
+
+    def to_Json(self):
+        myDict = {}
+        myDict["Producto"] = self.name
+        myDict["Cantidad"] = self.cantidad
+        myDict["Precio"] = self.precio
+        myDict["Id"] = self.id
+        myDict["ownerId"] = self.ownerId
+
+        return json.dumps(myDict)
+
