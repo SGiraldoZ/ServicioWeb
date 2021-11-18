e2b0ef2a846e20638f01c17b61cc645167092b77
SGiraldoZ <sebasgizu@gmail.com>
2021-11-17 19:17:08 -0500

    Product Controller with 3 endpoints

--- Producto/controller/productController.py
+++ Producto/controller/productController.py
@@ -1,33 +1,22 @@
 from flask import Blueprint, request, Response
-
+from ..service.productService import *
 
 product_controller = Blueprint("product",__name__)
 
 @product_controller.route('/inventarioDisp.json', methods=['GET'])
 def getAvailableProducts():
     userToken = request.args["user"]
-    return {"Products":getOwnerProds(userToken)}
-
-@product_controller.route("/productQt.json", methods=['GET'])
-def getProductQuantity():
-    userToken = request.args["user"]
-    prodId = request.args["product"]
-    return {"Products":getOwnerProd(userToken,prodId)}
+    return getOwnerProducts(userToken)
 
 @product_controller.route("/productInsert", methods = ['GET', 'POST'])
 def insertProducts():
     data = request.get_json()
-    if data:
-        if "nombre" in data:
-            nombre = data['nombre']
-        if "cantidad" in data:
-            cantidad = data['cantidad']
-        if "precio" in data:
-            precio = data['precio']
-        if "owner" in data:
-            owner = data['owner']
-        ownerId = getOwnerId(owner)
-        insertProduct(nombre, cantidad, precio, ownerId)
+    productCreated = createProduct(data)
+    if productCreated:
         return Response(status=200)
+    return Response(status=400)
 
-    return "<h1> Algo salio mal</h1>"
+@product_controller.route("/exists", methods = ['GET'])
+def productExists():
+    product = request.args["product"]
+    return {"Exists": product_exists_by_name(product)}
